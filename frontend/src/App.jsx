606c8bb2e8ec2a3837eb4b9e04c177cc37775dcb
Dave Ejezie <evergreendave1@gmail.com>
2024-09-06 20:59:50 +0100

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="Container-Fluid">
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
