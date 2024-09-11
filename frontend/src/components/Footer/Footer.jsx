3d49493f66606a6b5084df4c8c67acc18d734b37
Dave Ejezie <evergreendave1@gmail.com>
2024-09-11 16:05:21 +0100

    Social Links

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { SiCredly } from "react-icons/si";
 import "./Footer.scss";
+import logo from "../../assets/D.svg";
 
 
 const Footer = () => {
@@ -13,42 +16,38 @@ const Footer = () => {
             <ul className="footer-list">
               <li>
                 <a
-                  href="https://github.com/yourusername"
+                  href="https://github.com/davest9496"
                   target="_blank"
                   rel="noopener noreferrer"
-                 
                 >
-                  GitHub
+                  <FaGithub className="footer-icon" /> GitHub
                 </a>
               </li>
               <li>
                 <a
-                  href="https://linkedin.com/in/yourusername"
+                  href="https://www.linkedin.com/in/dave-ejezie-896798b9"
                   target="_blank"
                   rel="noopener noreferrer"
-                 
                 >
-                  LinkedIn
+                  <FaLinkedin className="footer-icon" /> LinkedIn
                 </a>
               </li>
               <li>
                 <a
-                  href="https://twitter.com/yourusername"
+                  href="https://x.com/DuEjezie"
                   target="_blank"
                   rel="noopener noreferrer"
-                 
                 >
-                  Twitter
+                  <FaTwitter className="footer-icon" /> Twitter
                 </a>
               </li>
               <li>
                 <a
-                  href="https://twitter.com/yourusername"
+                  href="https://www.credly.com/users/dave-ejezie"
                   target="_blank"
                   rel="noopener noreferrer"
-                 
                 >
-                  Credly
+                  <SiCredly className="footer-icon" /> Credly
                 </a>
               </li>
             </ul>
@@ -57,31 +56,29 @@ const Footer = () => {
             <h5 className="footer-title">Quick Links</h5>
             <ul className="footer-list">
               <li>
-                <Link to="/">
-                  Home
-                </Link>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to="/chat">
-                  Chat
-                </Link>
+                <Link to="/about">About</Link>
               </li>
               <li>
-                <Link to="/about">
-                  About
-                </Link>
+                <Link to="/portfolio">Portfolio</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact Me</Link>
               </li>
             </ul>
           </Col>
           <Col md={4}>
-            <h5 className="footer-title">Contact Us</h5>
-            <p>Email: info@aichatapp.com</p>
-            <p>Phone: (123) 456-7890</p>
+            <div className="footer-logo-container">
+              <img src={logo} alt="Logo image" className="footer-logo" />
+            </div>
           </Col>
         </Row>
+        <hr />
         <Row className="mt-3">
           <Col className="text-center">
-            <p>&copy; 2023 AI Chat App. All rights reserved.</p>
+            <p>&copy; 2024 Portfolio Website Version 2.0.0</p>
           </Col>
         </Row>
       </Container>
