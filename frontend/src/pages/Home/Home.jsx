23b958af39ef780446d0cd731dfdf381748cca07
Dave Ejezie <evergreendave1@gmail.com>
2024-09-25 14:22:13 +0100

    Responsive text

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,18 @@
+import { useCallback } from "react";
 import "./Home.scss";
-import { Link } from "react-router-dom";
 import Button from "../../components/Button/Button";
-// import { PiHandTapThin } from "react-icons/pi";
 import { PiArrowFatLinesDownThin } from "react-icons/pi";
 import About from "../About/About";
 import CallToAction from "../../components/CallToAction/CallToAction";
 
-
 function Home() {
+  const scrollToAbout = useCallback(() => {
+    const aboutSection = document.getElementById("about");
+    if (aboutSection) {
+      aboutSection.scrollIntoView({ behavior: "smooth" });
+    }
+  }, []);
+
   return (
     <div className="home">
       <div className="container">
@@ -15,36 +20,27 @@ function Home() {
           <div className="hero-content">
             <h1 className="hero-text">
               Hey, I am{" "}
-              <span>
+              <span className="hero-name">
                 Dave
                 <br /> Ejezie
               </span>{" "}
               and I<br /> love building pixel
               <br /> perfect websites!
             </h1>
-            <Link to="/about">
-              <Button
-                className="btn-primary align-bottom-left"
-                onClick={() => {
-                  const aboutSection = document.querySelector('.about');
-                  if (aboutSection) {
-                    aboutSection.scrollIntoView({ behavior: 'smooth' });
-                  }
-                }}
-                type="button"
-                disabled={false}
-              >
-                About Me
-                {/* <PiHandTapThin className="btn-icon" />
-                 */}
-                <PiArrowFatLinesDownThin className="btn-icon" />
-              </Button>
-            </Link>
+            <Button
+              className="btn-primary align-bottom-left"
+              onClick={scrollToAbout}
+              type="button"
+              disabled={false}
+            >
+              About Me
+              <PiArrowFatLinesDownThin className="btn-icon" />
+            </Button>
           </div>
         </section>
       </div>
       <div className="container">
-        <About />
+        <About id="about" />
         <CallToAction />
       </div>
     </div>
